8ea5a815008dc170efe99570ac0b8a7fc5d93bcd
Zach Barge <zachb.barge@gmail.com>
2023-12-26 16:25:52 -0500

    Began day 3 puzzle logic for determining if a symbol is adjacent to a number.

--- day_3/part_1/part_1.js
+++ day_3/part_1/part_1.js
@@ -7,23 +7,23 @@ import { input, testInput } from '../input.js';
  * @returns {number} Sum of mechanical part numbers
  */
 export function solve(problemInput) {
-  const symbols = '!@#$%^&*';
-  console.log(problemInput);
+	const symbols = '!@#$%^&*';
+	console.log(problemInput);
 
-  //Surrounding indices  of array[i][j]
-  //Left - array[i][j-1]
-  //Top Left - array[i-1][j-1]
-  //Top - array[i-1][j]
-  //Top Right - array[i+1][j+1]
-  //Right - array[i][j+1]
-  //Bottom Right - array[i+1][j+1]
-  //Bottom - array[i+1][j]
-  //Bottom Left - array [i+1][j-1]
+	//Surrounding indices  of array[i][j]
+	//Left - array[i][j-1]
+	//Top Left - array[i-1][j-1]
+	//Top - array[i-1][j]
+	//Top Right - array[i+1][j+1]
+	//Right - array[i][j+1]
+	//Bottom Right - array[i+1][j+1]
+	//Bottom - array[i+1][j]
+	//Bottom Left - array [i+1][j-1]
 
-  for (rowIndex in problemInput) {
-    for (columnIndex in row) {
-    }
-  }
+	for (rowIndex in problemInput) {
+		for (columnIndex in row) {
+		}
+	}
 }
 
 if (process.env.NODE_ENV !== 'test') console.log(solve(testInput));
